6ba060a49310cdf969e9e05e4b5e5e693aca5dd0
Rikhil Arora <rikhil.arora@check24.de>
2024-07-17 12:44:17 +0200

    feat: component creation and route created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.component'),
+  },
+];
